fb7bdea36170cd753cf03a12c9b29375b107d4d9
daima3629 <daima3629@usbc.be>
2020-10-25 14:06:26 +0900

    [Fix] fix some bugs

--- discord_status/wrapper.py
+++ discord_status/wrapper.py
@@ -4,23 +4,24 @@ import datetime
 import aiohttp
 
 API_URL = "https://srhpyqt94yxb.statuspage.io/api/v2/summary.json"
-TIME_FORMAT = re.compile(r"^(\d{4})-(\d{2})-(\d{2})T(\d{2}):({\d{2}}):({\d{2}}).(\d{3})-07:00$")
+TIME_FORMAT = re.compile(r"^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d{3})-07:00$")
 # ex: 2020-10-22T03:09:28.892-07:00
-TIMEZONE_TIJUANA = datetime.timedelta(hours=-7)
+TIMEZONE_TIJUANA = datetime.timezone(datetime.timedelta(hours=-7))
 
 
 class DiscordStatus:
     def __init__(self, base_json: dict):
-        pass
+        self._sort(base_json)
 
     @classmethod
     async def request(cls):
         async with aiohttp.ClientSession() as session:
             async with session.get(API_URL) as resp:
-                base_dict = json.loads(resp.text())
+                text = await resp.text()
+                base_dict = json.loads(text)
                 return cls(base_dict)
 
     def _sort(self, base_json: dict):
-        updated_time_re = TIME_FORMAT.match(base_json)
-        update_time = datetime.datetime(*map(int, updated_time_re.groups(0, 1, 2, 3, 4, 5, 6)), TIMEZONE_TIJUANA)
-        self.uptate_at = update_time
+        updated_time_re = TIME_FORMAT.match(base_json["page"]["updated_at"])
+        updated_time = datetime.datetime(*map(int, updated_time_re.groups()), TIMEZONE_TIJUANA)
+        self.updated_at = updated_time
